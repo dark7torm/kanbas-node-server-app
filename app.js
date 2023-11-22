68044f872a3bc4ba450a8d12013dc4b11cbf1f6e
Ren Taguchi <132172062+dark7torm@users.noreply.github.com>
2023-11-22 00:05:15 -0500

    fix

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ import cors from "cors"
 import "dotenv/config";
 // const express = require('express')
 const app = express()
-var cors = require('cors');
 app.use(cors());
 app.use(express.json());
 Lab5(app)

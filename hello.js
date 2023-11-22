bb94a5b71a99ad98efd5d0c836517252929f95e4
Ren Taguchi <132172062+dark7torm@users.noreply.github.com>
2023-11-22 00:08:18 -0500

    fix

--- hello.js
+++ hello.js
@@ -1,9 +1,3 @@
-app.use((req, res, next) => {
-   res.setHeader("Access-Control-Allow-Origin", "*");
-   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-   next();
- })
 const Hello = (app) => {
 app.get('/hello', (req, res) => {
     res.send('Life is good!')
